jakecoffman/cp

commit f1e35a6b934d3f5e94f4befe6939ebfedaf0c627

modernize for loops 2 (manual)

space.go
@@ -595,7 +595,7 @@ func (space *Space) ProcessComponents(dt float64) {
 		}
 
 		// Generate components and deactivate sleeping ones
-		for i := 0; i < len(space.dynamicBodies); {
+		for i := range space.dynamicBodies {
 			body := space.dynamicBodies[i]
 
 			if body.ComponentRoot() == nil {
@@ -794,7 +794,7 @@ func (space *Space) Unlock(runPostStep bool) {
 		return
 	}
 
-	for i := 0; i < len(space.rousedBodies); i++ {
+	for i := range space.rousedBodies {
 		space.Activate(space.rousedBodies[i])
 		space.rousedBodies[i] = nil
 	}
@@ -932,7 +932,7 @@ func (space *Space) EachShape(f func(*Shape)) {
 func (space *Space) EachConstraint(f func(*Constraint)) {
 	space.Lock()
 
-	for i := 0; i < len(space.constraints); i++ {
+	for i := range space.constraints {
 		f(space.constraints[i])
 	}
 
@@ -1063,7 +1063,7 @@ func (space *Space) TimeStep() float64 {
 }
 
 func (space *Space) PostStepCallback(key any) *PostStepCallback {
-	for i := 0; i < len(space.postStepCallbacks); i++ {
+	for i := range space.postStepCallbacks {
 		callback := space.postStepCallbacks[i]
 		if callback != nil && callback.key == key {
 			return callback

arbiter.go
@@ -115,7 +115,7 @@ func (arbiter *Arbiter) ApplyCachedImpulse(dt_coef float64) {
 		return
 	}
 
-	for i := 0; i < arbiter.count; i++ {
+	for i := range arbiter.count {
 		contact := arbiter.contacts[i]
 		j := arbiter.n.Rotate(Vector{contact.jnAcc, contact.jtAcc})
 		apply_impulses(arbiter.body_a, arbiter.body_b, contact.r1, contact.r2, j.Mult(dt_coef))
@@ -129,7 +129,7 @@ func (arbiter *Arbiter) ApplyImpulse() {
 	surface_vr := arbiter.surface_vr
 	friction := arbiter.u
 
-	for i := 0; i < arbiter.count; i++ {
+	for i := range arbiter.count {
 		con := &arbiter.contacts[i]
 		nMass := con.nMass
 		r1 := con.r1
@@ -171,7 +171,7 @@ func (arb *Arbiter) PreStep(dt, slop, bias float64) {
 	n := arb.n
 	bodyDelta := b.p.Sub(a.p)
 
-	for i := 0; i < arb.count; i++ {
+	for i := range arb.count {
 		con := &arb.contacts[i]
 
 		// Calculate the mass normal and mass tangent.
@@ -199,7 +199,7 @@ func (arb *Arbiter) Update(info *CollisionInfo, space *Space) {
 	arb.body_b = b.body
 
 	// Iterate over the possible pairs to look for hash value matches.
-	for i := 0; i < info.count; i++ {
+	for i := range info.count {
 		con := &info.arr[i]
 
 		// r1 and r2 store absolute offsets at init time.
@@ -483,7 +483,7 @@ func (arb *Arbiter) ContactPointSet() ContactPointSet {
 		set.Normal = n
 	}
 
-	for i := 0; i < set.Count; i++ {
+	for i := range set.Count {
 		// Contact points are relative to body CoGs;
 		p1 := arb.body_a.p.Add(arb.contacts[i].r1)
 		p2 := arb.body_b.p.Add(arb.contacts[i].r2)

body.go
@@ -395,7 +395,7 @@ func (body *Body) Activate() {
 			bodyToo = next
 		}
 
-		for i := 0; i < len(space.sleepingComponents); i++ {
+		for i := range space.sleepingComponents {
 			if space.sleepingComponents[i] == root {
 				space.sleepingComponents = append(space.sleepingComponents[:i], space.sleepingComponents[i+1:]...)
 				break
@@ -593,7 +593,7 @@ func (body *Body) EachArbiter(f func(*Arbiter)) {
 
 // EachShape calls f once for each shape attached to this body
 func (body *Body) EachShape(f func(*Shape)) {
-	for i := 0; i < len(body.shapeList); i++ {
+	for i := range body.shapeList {
 		f(body.shapeList[i])
 	}
 }

draw.go
@@ -142,7 +142,7 @@ func DrawConstraint(constraint *Constraint, options Drawer) {
 			verts = append(verts, Vector{v.Dot(r1) + a.X, v.Dot(r2) + a.Y})
 		}
 
-		for i := 0; i < len(springVerts)-1; i++ {
+		for i := range len(springVerts) - 1 {
 			options.DrawSegment(verts[i], verts[i+1], color, data)
 		}
 	// these aren't drawn in Chipmunk, so they aren't drawn here

shape.go
@@ -253,7 +253,7 @@ func ShapesCollide(a, b *Shape) ContactPointSet {
 		set.Normal = info.n
 	}
 
-	for i := 0; i < info.count; i++ {
+	for i := range info.count {
 		p1 := contacts[i].r1
 		p2 := contacts[i].r2
 

spacehash.go
@@ -378,7 +378,7 @@ func (hash *SpaceHash) clearTableCell(idx int) {
 }
 
 func (hash *SpaceHash) clearTable() {
-	for i := 0; i < hash.numCells; i++ {
+	for i := range hash.numCells {
 		hash.clearTableCell(i)
 	}
 }

commit 7d79eefadb7848c64b394a33da82f276dd2c08a5

Fix index out of range panic in ProcessComponents
This fixes a regression introduced in commit f1e35a6 (modernize for loops 2).

When modernizing the for loop from traditional style to range syntax,
the manual i++ increment was not removed, causing a double-increment bug.

The for..range statement automatically increments the index variable,
so the manual i++ at line 606 caused the loop to skip indices and
eventually panic with 'index out of range'.

Fixes the panic that occurs when SleepTimeThreshold != INFINITY.

space.go
@@ -603,8 +603,6 @@ func (space *Space) ProcessComponents(dt float64) {
 				}
 			}
 
-			i++
-
 			// Only sleeping bodies retain their component node pointers.
 			body.sleepingRoot = nil
 			body.sleepingNext = nil